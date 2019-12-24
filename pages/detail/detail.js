3c84b66324d2f9cd917ee2beb7750e8fe10d6257
KLong <15110232612@163.com>
2019-12-24 18:49:27 +0800

    11

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,6 +1,9 @@
 // pages/detail/detail.js
 let datas = require('../../datas/list-data.js');
 console.log(datas, typeof datas)
+// 获取全局App的实例
+let appDatas = getApp();
+console.log(appDatas)
 
 
 Page({
@@ -10,7 +13,8 @@ Page({
    */
   data: {
     detailObj: {},
-    icCollected: false, //标识当前文章是否被收藏，默认是未收藏
+    isCollected: false, // 标识当前文章是否被收藏，默认是未收藏
+    isMusicPlay: false, // 标识音乐是否播放，默认未播放
   },
 
   /**
@@ -24,14 +28,89 @@ Page({
       detailObj: datas.list_data[index],
       index
     })
-  },
-  //读取本地缓存的收藏状态数据
-  let oldStorage = wx.getStorageSync('isCollected'),
 
-  if(oldStorage[index]){
-    this.setData({
-      
+    // 读取本地缓存的收藏状态数据
+    let oldStorage = wx.getStorageSync('isCollected')
+    // 更细状态数据
+    if (oldStorage[index]) {
+      this.setData({
+        isCollected: oldStorage[index]
+      })
+    }
+
+
+    // 处理音乐播放的状态
+    let { isPlay, pageIndex } = appDatas.globalData
+    if (isPlay && pageIndex === index) {
+      // 修改音乐播放的状态为true
+      this.setData({
+        isMusicPlay: true
+      })
+    }
+
+    // 监听音乐播放
+    wx.onBackgroundAudioStop(() => {
+      console.log('音乐停止')
+      this.setData({
+        isMusicPlay: false
+      })
+
+      appDatas.globalData.isPlay = false
     })
+
+  },
+
+  // 定义收藏功能的方法
+  handleCollection() {
+    // 修改是否收藏的状态isCollected
+    let isCollected = !this.data.isCollected
+    this.setData({
+      isCollected
+    })
+
+    // 显示消息提示框
+    let title = isCollected ? '收藏成功' : '取消收藏'
+    wx.showToast({
+      title
+    })
+
+    // 将收藏的状态存储到本地
+    // 问题: 多个页面共享一个状态数据 
+    // wx.setStorageSync('isCollected', isCollected)
+    // 解决思路： 添加状态的同时添加页面标识
+    // 对象： {0: true, 2: false, 3: true}
+    let index = this.data.index
+    //问题： 收藏的状态永远只有一个键值对
+    // let obj = {}
+    // 解决思路： 获取之前的收藏的对象
+    let obj = wx.getStorageSync('isCollected') || {}
+    obj[index] = isCollected
+    wx.setStorageSync('isCollected', obj)
+  },
+
+  // 处理音乐播放的功能函数
+  handleMusicPlay() {
+
+    let isMusicPlay = !this.data.isMusicPlay
+    this.setData({
+      isMusicPlay
+    })
+
+    // 音乐播放功能实现
+    let { dataUrl, title, coverImgUrl } = this.data.detailObj.music
+    if (isMusicPlay) { // 音乐播放
+      wx.playBackgroundAudio({
+        dataUrl,
+        title,
+        coverImgUrl
+      })
+      // 将当前播放的状态存入全局的实例中
+      appDatas.globalData.isPlay = true
+      appDatas.globalData.pageIndex = this.data.index
+    } else {// 音乐停止
+      wx.stopBackgroundAudio()
+      appDatas.globalData.isPlay = false
+    }
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,39 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg:'岁月如歌' 
+    msg: 'Hello',
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onload')
-    // setTimeout(()=>{
+    console.log('onLoad')
+    // 发送ajax请求，前后端交互
+    // console.log(this.data.msg) // 当前页面的实例对象
+    // data中的 数据没有进行数据劫持和代理
+    // 修改状态数据
+    // vue: this.xxx = value
+    // React： this.setState()
+    // 小程序： this.setData()
+    // setTimeout(() => {
     //   this.setData({
-    //     msg:'修改之后的数据'
+    //     msg: '修改之后的数据'
     //   })
+    //   // 修改的动作：同步
     //   console.log(this.data.msg)
-    // },2000)
-  },
-  //处理事件的回调
-  toList(){
-    wx.navigateTo({
-      url:'/pages/list/list'
+    // }, 2000)
+
+
+
+
+
+    // 获取用户信息, 用户授权之后
+    wx.getUserInfo({
+      success: (res) => {
+        
+        this.setData({
+          userInfo: res.userInfo
+        })
+      },
+      fail: () => {
+        console.log('获取用户信息失败')
+      }
     })
+
+
   },
+  // 处理事件的回调
+  toList() {
+    // 跳转至list页面
+    wx.reLaunch({
+      url: '/pages/list/list'
+    })
+
+  },
+  // 为了第一次用户授权，并且成功获取用户数据
+  handleGetUserInfo(res) {
+    console.log(res)
+    if (res.detail.userInfo) {
+      // 用户授权了
+      this.setData({
+        userInfo: res.detail.userInfo
+      })
+    }
+  },
+
+  // handleChild() {
+  //   console.log('child')
+  // },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.log('onready')
+    console.log('onReady')
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.log('onshow')
+    console.log('onShow')
+
   },
 
   /**
